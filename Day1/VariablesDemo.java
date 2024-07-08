f1bcf631f6fb8b061fb111264b8bc4099732a35c
Hansin Patwa <77282604+Hansin2901@users.noreply.github.com>
2024-07-08 20:46:11 +0530

    Added Day1 content

---
+++ Day1/VariablesDemo.java
@@ -0,0 +1,27 @@
+public class VariablesDemo {
+
+    public static void main(String[] args) {
+
+        int number = 10; // Integer
+
+        double pi = 3.14; // Double
+
+        char letter = 'A'; // Character
+
+        boolean isJavaFun = true; // Boolean
+
+        String greeting = "Hello"; // String
+
+        System.out.println(number);
+
+        System.out.println(pi);
+
+        System.out.println(letter);
+
+        System.out.println(isJavaFun);
+
+        System.out.println(greeting);
+
+    }
+
+}
